ae855aace194a4fc32062d41c89498062177d22c
CAUADEV <cauadev@gmail.com>
2021-04-25 11:01:02 -0300

    setup inicial do projeto.

---
+++ src/test/java/com/cauadev/hotelcalifornia/HotelCaliforniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cauadev.hotelcalifornia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelCaliforniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
